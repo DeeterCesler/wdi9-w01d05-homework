345be9f27b2e46a4a798da5788745d6132618280
Deeter <deeter.cesler@gmail.com>
2018-09-14 15:36:06 -0600

    give me five

--- app.js
+++ app.js
@@ -77,9 +77,18 @@
                 for(let i=12; i<=4000; i+=3){
                     console.log(i)
                 };
-        // B.
+        // B. Get Even
                 for(let i=1 ; i<=100; i++){
                     if(i%2===0){
                         console.log(`${i} <-- is an even number`)
+                    }
+                }
+        // C. Give me Five
+                for(let i=0; i<=100; i++){
+                    if(i%5===0){
+                        console.log(`I found a ${i}. High five!`)
+                    }
+                    if(i%3===0){
+                        console.log(`I found a ${i}. Three is a crowd`)
                     }
                 }
