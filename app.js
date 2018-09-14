50d30702ac22318d38923160c69e8bc5ef43dda1
Deeter <deeter.cesler@gmail.com>
2018-09-14 15:53:14 -0600

    change values

--- app.js
+++ app.js
@@ -119,3 +119,8 @@
         randomThings[0];
         randomThings[2] = "World";
         console.log(randomThings);
+    // D. Change Values
+        const ourClass = ["Salty", "Zoom", "Sardine", "Slack", "Github"]
+        ourClass[2];
+        ourClass[4] = "Octocat";
+        ourClass.push("Cloud City");
