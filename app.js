8b50ce54f4056b1c096dc2f30c5ff31c928206d9
Deeter <deeter.cesler@gmail.com>
2018-09-14 15:05:40 -0600

    question 1 answers

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 console.log("it's live")
+
+// 1. Variable & Datatypes
+    // A. Q + A
+        // How do we assign a value to a variable?
+            // const variable = value;
+        // How do we change the value of a variable?
+            // varible = newValue;
+        // How do we assign an existing variable to a new variable?
+            // newVariable = existingVariable;
+        // Remind me, what are declare, assign, and define?
+            // declare is when you establish a new variable
+            // assign is when you give that variable a value
+            // define is when you re-assign a value
+        // What is pseudocoding and why should you do it?
+            // it's writing out what you want your code to do in plain english to clarify what you'll actually work on
+        // What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+            // roughly 75% of time planning, 25% of time typing
+
