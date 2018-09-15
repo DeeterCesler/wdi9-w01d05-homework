e800e68cb9c717db24b836c6ff48fcb431a7ad51
Deeter <deeter.cesler@gmail.com>
2018-09-15 12:54:21 -0600

    objects e

--- app.js
+++ app.js
@@ -330,3 +330,16 @@
         user.purchased.push("peace of mind");
         user.purchased.push("Merino jodhpurs");
         console.log(user.purchased[2]);
+    // E.
+        user.friend = {
+            name: "dan",
+            age: 23,
+            location: "Brooklyn",
+            purchased: []
+        };
+        console.log(user.friend.name);
+        console.log(user.friend.location);
+        user.friend.age = 55;
+        user.friend.purchased.push("The One Ring");
+        user.friend.purchased.push("A latte");
+        console.log(user.friend.purchased[1]);
